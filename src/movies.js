e7a317fe3eb876c4442598c5706041eb12d9ab7a
Sebastian Mottschall <srophkor@gmail.com>
2023-01-20 16:35:37 +0100

    Cleaned up a bit

--- src/movies.js
+++ src/movies.js
@@ -2,57 +2,6 @@
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
 
-const moviesTestArray = [
-    {
-      title: 'The Shawshank Redemption',
-      year: 1993,
-      director: 'Frank Darabont',
-      duration: '2h',
-      genre: ['Crime', 'Drama'],
-      score: 9.3
-    },
-    {
-      title: 'The Godfather',
-      year: 1972,
-      director: 'Francis Ford Coppola',
-      duration: '2h',
-      genre: ['Crime', 'Drama'],
-      score: 9.2
-    },
-    {
-      title: 'The Godfather: Part II',
-      year: 1974,
-      director: 'Francis Ford Coppola',
-      duration: '3h 22min',
-      genre: ['Crime', 'Drama'],
-      score: 9
-    },
-    {
-      title: 'The Dark Knight',
-      year: 2008,
-      director: 'Christopher Nolan',
-      duration: '2h 32min',
-      genre: ['Action', 'Crime', 'Drama', 'Thriller'],
-      score: 9
-    },
-    {
-      title: '12 Angry Men',
-      year: 1957,
-      director: 'Sidney Lumet',
-      duration: '1h 36min',
-      genre: ['Crime', 'Drama'],
-      score: 8.9
-    },
-    {
-      title: 'Schindler"s List',
-      year: 1993,
-      director: 'Steven Spielberg',
-      duration: '3h 15min',
-      genre: ['Biography', 'Drama', 'History'],
-      score: 8.9
-    },
-]
-
 function getAllDirectors(moviesArray) {
     return moviesArray.map(movie => {
         return movie.director;
